31907bd404a6e0041d8c2c4d965a7198ed32d12e
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2024-11-17 20:21:03 +0700

    config application.yaml

--- profile-service/src/main/java/com/kanban/profile/ProfileServiceApplication.java
+++ profile-service/src/main/java/com/kanban/profile/ProfileServiceApplication.java
@@ -2,8 +2,12 @@ package com.kanban.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
+@EnableFeignClients
 public class ProfileServiceApplication {
 
 	public static void main(String[] args) {
